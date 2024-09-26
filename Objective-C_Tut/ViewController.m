30b3f889e5f05b9cccaef99a6c1b4ba0c30cce6a
Sanket Mule <sanketsp60@gmail.com>
2024-09-26 13:36:44 +0530

    New changes

--- Objective-C_Tut/ViewController.m
+++ Objective-C_Tut/ViewController.m
@@ -17,13 +17,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // Using the Person class (with extension)
     Person *person = [[Person alloc]init];
     person.name = @"Sanket Mule";
     person.age = 24;
     [person printPersonDetails];
     
-    NSString *sentence = @"Hello World";
-    NSLog(@"Word count is: %ld");
+    // Using the NSString Category
+    NSString *sentence = @"Objective-C is fun and powerful";
+        NSLog(@"Word count: %ld", (long)[sentence wordCount]);  
 }
 
 
